824032d030d38170881b80795d8e18538c2cba65
Frederico Espeschit <fespeschitt@gmail.com>
2022-06-22 15:34:18 -0300

    Resolução da questão 1, 2 e 3, no arquivo script

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,34 @@
+//1- Resposta
+const a = 10;
+const b = 50;
+
+//Adição
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//2- Resposta
+const c = 500;
+const d = 700;
+
+if(c > d){
+    console.log('C é maior que D')
+} else {
+    console.log('D é maior que C')
+}
+
+//3 - Resposta
+const e = 500;
+const f = 6000;
+const g = 700;
+
+if(e > f && e > g){
+    console.log(e)
+} else if(f > e && f > g){
+    console.log(f)
+} else {
+    console.log(g)
+}
+
