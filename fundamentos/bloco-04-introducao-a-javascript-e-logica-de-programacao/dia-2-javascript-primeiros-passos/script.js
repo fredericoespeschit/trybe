cbb1c7d4beb80e4e43263e4d88d59dfd6c6acbdc
Frederico Espeschit <fespeschitt@gmail.com>
2022-07-02 22:58:45 -0300

    Subindo resposta exercicio 6, 8 e 9

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-primeiros-passos/script.js
@@ -22,7 +22,7 @@ let mediaAritmetica = 0;
 let tamanhoArray = numbers3.length;
 for (let contador = 0; contador < numbers3.length; contador++) {
 
-    mediaAritmetica += (numbers3[contador]) / tamanhoArray
+    mediaAritmetica += (numbers3[contador]) / tamanhoArray;
 }
 
 console.log(mediaAritmetica);
@@ -34,7 +34,7 @@ let numbers4 = [1, 9, 3, 19, 70, 8, 20, 2, 35, 27];
 let mediaAritmetica4 = 0;
 let tamanhoArray4 = numbers4.length;
 for (let contador = 0; contador < numbers4.length; contador++) {
-    mediaAritmetica4 += (numbers4[contador]) / tamanhoArray4
+    mediaAritmetica4 += (numbers4[contador]) / tamanhoArray4;
 }
 
 if (mediaAritmetica4 > 20) {
@@ -56,3 +56,31 @@ for (let contador = 0; contador < numbers5.length; contador++) {
 
 console.log(maiorValor);
 
+//6 - Resposta
+let numberx = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(let indice = 0; indice < numberx.length; indice = indice + 1){
+    if(numberx[indice] % 2 === 1){
+        console.log('impar: ' + numberx[indice]);
+    }
+}
+
+//7 - Resposta
+
+
+//8 - Resposta
+let array = [];
+for(let index = 1; index <= 25; index = index +1){
+   // console.log(index);
+    array.push(index);
+}
+
+console.log(array);
+
+//9 - Resposta
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2
+let result;
+for (let i = 0; i < array.length; i = i +1){
+    //console.log(array[i]);
+    result = array[i] / 2;
+    console.log('Exercicio 9: ' + result);
+}
