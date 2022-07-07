4380d706718f60f19ee6fde653ab90530854fab2
Frederico Espeschit <fespeschitt@gmail.com>
2022-07-07 16:02:21 -0300

    botão para limpar as informações contidas nos campos do forms

--- fundamentos/bloco-06-html-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
+++ fundamentos/bloco-06-html-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
@@ -1,8 +1,6 @@
 const BUTTON = document.querySelector('#buttonSubmit');
-const BUTTON_CLEAR = document.querySelector('#buttonSubmitClear');
 
 function buttonSubmit(eventButtonSubmit){
     eventButtonSubmit.preventDefault();
 }
 BUTTON.addEventListener('click', buttonSubmit);
-BUTTON_CLEAR.addEventListener('click', buttonSubmit)
