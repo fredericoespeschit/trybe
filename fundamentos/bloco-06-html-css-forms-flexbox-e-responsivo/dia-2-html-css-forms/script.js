a1e6e0df29d2d25411882b4811f2d148c798f3c3
Frederico Espeschit <fespeschitt@gmail.com>
2022-07-08 02:13:58 -0300

    biblioteca JavaScript de date picker[DatePickerX]

--- fundamentos/bloco-06-html-css-forms-flexbox-e-responsivo/dia-2-html-css-forms/script.js
+++ fundamentos/bloco-06-html-css-forms-flexbox-e-responsivo/dia-2-html-css-forms/script.js
@@ -1,12 +1,39 @@
 const BUTTON = document.querySelector('#buttonSubmit');
 const CHECKBOX_DOIS = document.querySelector('#checkbox-2');
 
-function buttonSubmit(eventButtonSubmit){
+
+function buttonSubmit(eventButtonSubmit) {
     eventButtonSubmit.preventDefault();
 }
 BUTTON.addEventListener('click', buttonSubmit);
 
-function checkboxDois (){
+function checkboxDois() {
     console.log(CHECKBOX_DOIS);
 }
 CHECKBOX_DOIS.addEventListener('click', checkboxDois);
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    var $min = document.querySelector('.real [name="realDPX-min"]'),
+        $max = document.querySelector('.real [name="realDPX-max"]');
+
+    $min.DatePickerX.init({
+        mondayFirst: true,
+        format: 'dd/mm/yyyy',
+        //minDate: new Date(2021, 5, 9),
+        maxDate: $max
+    });
+
+    $max.DatePickerX.init({
+        mondayFirst: true,
+         //minDate: $min,
+        format: 'dd/mm/yyyy',
+       /*
+        maxDate: function () {
+            var date = new Date();
+            return new Date().setDate(date.getDate() + 1);
+        }, */
+        clearButton: false
+    });
+
+});
